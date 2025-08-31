eafbae4292044c1b99b0cb3276c753fab68d47b9
Blessing James <blessingjam9@gmail.comgit config --listgit config --global user.email blessingjam9@gmail.com>
2025-08-31 08:15:00 +0100

    Add changes

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import WelcomeMessage from './components/WelcomeMessage';
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
 import './App.css';
 
 function App() {
   return (
     <div>
       <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <Footer />
+
     </div>
   );
 }
