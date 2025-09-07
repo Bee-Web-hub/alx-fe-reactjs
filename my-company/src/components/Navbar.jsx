32cd91d20da27992056271ec1474258fd2ab7d2c
Blessing James <blessingjam9@gmail.comgit config --listgit config --global user.email blessingjam9@gmail.com>
2025-09-07 22:42:03 +0100

    Make changes

---
+++ my-company/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+// src/components/Navbar.jsx
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav style={{ padding: "10px", backgroundColor: "#333", color: "#fff" }}>
+      <Link to="/" style={{ color: "#fff", margin: "0 10px", textDecoration: "none" }}>Home</Link>
+      <Link to="/about" style={{ color: "#fff", margin: "0 10px", textDecoration: "none" }}>About</Link>
+      <Link to="/services" style={{ color: "#fff", margin: "0 10px", textDecoration: "none" }}>Services</Link>
+      <Link to="/contact" style={{ color: "#fff", margin: "0 10px", textDecoration: "none" }}>Contact</Link>
+    </nav>
+  );
+}
+
+export default Navbar;
