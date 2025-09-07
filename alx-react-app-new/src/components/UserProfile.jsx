c94bbc715abbed07200c4cfce58790a5104f6738
Blessing James <blessingjam9@gmail.comgit config --listgit config --global user.email blessingjam9@gmail.com>
2025-09-07 19:20:42 +0100

    Make changes

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -1,10 +1,17 @@
 // src/components/UserProfile.jsx
-const UserProfile = (props) => {
+import React, { useContext } from "react";
+import UserContext from "./UserContext";
+
+const UserProfile = () => {
+  const userData = useContext(UserContext); // get data from context
+
   return (
-    <div style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
-      <h2 style={{ color: 'blue' }}> {props.name}</h2>
-      <p>Age:<span style={{ fontWeight: 'bold' }}> </span>  {props.age}</p>
-      <p>Bio: {props.bio}</p>
+    <div style={{ border: "1px solid gray", padding: "10px", margin: "10px" }}>
+      <h2 style={{ color: "blue" }}>{userData.name}</h2>
+      <p>
+        Age: <span style={{ fontWeight: "bold" }}>{userData.age}</span>
+      </p>
+      <p>Bio: {userData.bio}</p>
     </div>
   );
 };
