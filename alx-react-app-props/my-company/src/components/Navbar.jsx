16632a73d486cbfd62b7e3708d5ff8e33ae10804
Blessing James <blessingjam9@gmail.comgit config --listgit config --global user.email blessingjam9@gmail.com>
2025-09-07 21:42:59 +0100

    Make changes

---
+++ alx-react-app-props/my-company/src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+// src/components/Navbar.jsx
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  const navStyle = {
+    display: "flex",
+    gap: "20px",
+    padding: "10px",
+    backgroundColor: "#f0f0f0",
+    marginBottom: "20px",
+  };
+
+  return (
+    <nav style={navStyle}>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/services">Services</Link>
+      <Link to="/contact">Contact</Link>
+    </nav>
+  );
+}
+
+export default Navbar;
